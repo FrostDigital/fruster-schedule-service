b0f14478ab6a739d75b6a389fba30a968d2b7aa7
Joel Söderström <joel.soderstrom@gmail.com>
2017-03-13 18:58:23 +0100

    Feature: Remove job (#2)
    * Makes sync interval (when to sync jobs from db) configurable
    
    * Adds initial state and option to remove (cancel) job
    
    * Remove job

---
+++ spec/RemoveJobHandler.spec.js
@@ -0,0 +1,60 @@
+const mongo = require("mongodb");
+const testUtils = require("fruster-test-utils");
+const bus = require("fruster-bus");
+const scheduleService = require("../schedule-service");
+const constants = require("../constants");
+const fixtures = require("./support/fixtures");
+
+describe("RemoveJobHandler", () => {
+
+	testUtils.startBeforeAll({
+		mongoUrl: "mongodb://localhost:27017/schedule-service-test",
+		service: scheduleService,
+		bus: bus,
+		afterStart: (connection) => {
+			return Promise.resolve();
+		}
+	});
+
+	it("should fail validation job", (done) => {
+		bus.request(constants.exposing.removeJob, {
+			req: "reqId",
+			data: {}
+		})
+		.catch(err => {			
+			expect(err).toBeError("BAD_REQUEST");
+			done();
+		});
+	});
+
+	it("should fail to remove job that does not exist", (done) => {
+		bus.request(constants.exposing.removeJob, {
+			req: "reqId",
+			data: {
+				id: "foooo"
+			}
+		})
+		.catch(err => {			
+			expect(err).toBeError("NOT_FOUND");
+			done();
+		});
+	});
+
+	it("should remove a job", (done) => {
+		const jobReq = fixtures.jobReq();
+		const removeJobReq = fixtures.removeJobReq({
+			id: jobReq.data.id
+		});
+
+		bus.request(constants.exposing.createJob, jobReq)
+			.then(() => bus.request(constants.exposing.removeJob, jobReq))			
+			.then(resp => {							
+				expect(resp.status).toBe(200);			
+				expect(resp.data.id).toBe(jobReq.data.id);			
+				expect(resp.data.subject).toBe(jobReq.data.subject);			
+				expect(resp.data.cron).toBe(jobReq.data.cron);			
+				done();
+			});
+	});
+
+});
