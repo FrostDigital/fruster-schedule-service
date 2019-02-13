865aab335c0953e33bb7fdfe192cc125212fe7a6
Dinuka Thilanga <dinuka@frostdigital.se>
2019-02-13 14:51:17 +0530

    update packages (#11)
    * update packages
    
    * refactor code

--- schedule-service.js
+++ schedule-service.js
@@ -1,4 +1,5 @@
 const bus = require("fruster-bus");
+const log = require("fruster-log");
 const mongo = require("mongodb");
 const constants = require("./constants");
 const JobRepo = require("./lib/repos/JobRepo");
@@ -7,38 +8,38 @@ const RemoveJobHandler = require("./lib/RemoveJobHandler");
 const CronRunner = require("./lib/cron/CronRunner");
 const conf = require("./conf");
 
-module.exports.start = (busAddress, mongoUrl) => {
+module.exports.start = (busAddress, mongoUrl) => {
 
 	return bus.connect(busAddress)
 		.then(() => mongo.connect(mongoUrl))
 		.then((db) => createIndexes(db))
-		.then((db) =>  {
+		.then((db) => {
 			let jobRepo = new JobRepo(db);
-			
+
 			let cronRunner = new CronRunner(jobRepo, {
 				start: true,
 				syncInterval: conf.syncInterval,
 				maxFailures: conf.maxFailures
 			});
-			
+
 			let createJob = new CreateJobHandler(jobRepo, cronRunner);
 			let removeJob = new RemoveJobHandler(jobRepo, cronRunner);
-						
+
 			bus.subscribe(constants.exposing.createJob, req => createJob.handle(req));
 			bus.subscribe(constants.exposing.removeJob, req => removeJob.handle(req));
 		});
 };
 
-
-async function createIndexes(db) {
+async function createIndexes(db) {
 	try {
-		await db.collection(constants.collections.jobs).createIndex({id: 1}, true);
-		await db.collection(constants.collections.invocations).createIndex({id: 1}, true);        
-		await db.collection(constants.collections.invocations).createIndex({created: 1}, {
+		await db.collection(constants.collections.jobs).createIndex({ id: 1 }, true);
+		await db.collection(constants.collections.invocations).createIndex({ id: 1 }, true);
+		await db.collection(constants.collections.invocations).createIndex({ created: 1 }, {
 			expireAfterSeconds: conf.invocationsTTL / 1000
 		});
-	} catch(err) {
+	} catch (err) {
 		log.warn("Failed creating db indexes", err);
 	}
+
 	return db;
 }
