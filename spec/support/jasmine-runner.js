62515313283b77df267a66abcf9d852094fb4f7c
Dinuka Thilanga <dinuka@frostdigital.se>
2019-02-13 10:29:32 +0530

    update packages

--- spec/support/jasmine-runner.js
+++ spec/support/jasmine-runner.js
@@ -1,13 +1,13 @@
 const errors = require("../../lib/errors");
 const Jasmine = require('jasmine');
-const SpecReporter = require('jasmine-spec-reporter');
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;;
 
-const noop = function() {};
+const noop = function () { };
 const jrunner = new Jasmine();
 
 require("fruster-custom-matchers")(errors); // add custom matchers once jrunner exists
 
-jrunner.configureDefaultReporter({print: noop});    // remove default reporter logs
+jrunner.configureDefaultReporter({ print: noop });    // remove default reporter logs
 jasmine.getEnv().addReporter(new SpecReporter());   // add jasmine-spec-reporter
 jrunner.loadConfigFile();                           // load jasmine.json configuration
 jrunner.execute();
