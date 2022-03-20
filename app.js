575882b1c29ddb9635f46ae2e279f6981dd7edba
Joel Soderstrom <joel.soderstrom@gmail.com>
2022-03-20 22:49:38 +0100

    fix: Uses same bus instance for fruster-health

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const log = require("fruster-log");
 const scheduleService = require("./schedule-service");
 const conf = require("./conf");
+const bus = require("fruster-bus");
 
-require("fruster-health").start();
+require("fruster-health").start(bus);
 
 scheduleService
 	.start(conf.bus, conf.mongoUrl)
 	.then(() => {
 		log.info(`Schedule service started and connected to bus ${conf.bus}`);
 	})
-	.catch((err) => {
+	.catch((err) => {
 		log.error("Failed starting schedule service", err);
 		process.exit(1);
 	});
